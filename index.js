c569c63c716aa2e4b6c31116bce67270e8232dc5
ishita <guptaishita242@gmail.com>
2022-12-11 11:22:32 +0000

    super-over-league

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <div className="image-container">
+      <h1 className="text">Super Over League </h1>
+    <div>
+      <img
+        className = "image-1"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      ></img>
+      <img
+        className = "image-1"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      ></img>
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
